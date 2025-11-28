5daa14d8f580ca09a62b4b9a02fd64f9a5973ef2
Aleksandr Kisel <51911663+Alex991995@users.noreply.github.com>
2025-11-28 14:54:44 +0300

    feat: add header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
 import { Layout } from '@layout/layout';
+import { Category } from '@pages/category/category';
 
 export const routes: Routes = [
   {
     path: '',
     component: Layout,
-    children: [],
+    children: [
+      { path: '', component: Category },
+      {
+        path: 'images',
+        loadComponent: () => import('@pages/images/images').then((m) => m.Images),
+      },
+      {
+        path: 'favourites',
+        loadComponent: () => import('@pages/favourites/favourites').then((m) => m.Favourites),
+      },
+    ],
   },
 ];
