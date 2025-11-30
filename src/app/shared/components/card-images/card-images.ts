1b8a5c91c1f46a4798238625ca0e97eaf4cbcabe
Aleksandr Kisel <51911663+Alex991995@users.noreply.github.com>
2025-11-30 14:00:14 +0300

    feat: add set up api call for topics and desplay tham

---
+++ src/app/shared/components/card-images/card-images.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-images',
+  imports: [],
+  templateUrl: './card-images.html',
+  styleUrl: './card-images.css',
+})
+export class CardImages {
+
+}
---
+++ src/app/shared/constants/array-topics.ts
@@ -0,0 +1,14 @@
+export const arrayTopics = [
+  { name: 'Art', slug: 'art' },
+  { name: 'Cars', slug: 'car' },
+  { name: 'Architecture', slug: 'architecture-interior' },
+  { name: 'Food', slug: 'food-drink' },
+  { name: 'Religion', slug: 'religion' },
+  { name: 'Clothes', slug: 'fashion' },
+  { name: 'Technologies', slug: 'technology' },
+  { name: 'Music', slug: 'music' },
+  { name: 'Business', slug: 'business' },
+  { name: 'Sport', slug: 'sport' },
+  { name: 'Social', slug: 'people' },
+  { name: 'Sky', slug: 'sky' },
+];
